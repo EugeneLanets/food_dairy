e3c9524985dc59a659e1a0494d956100accd62bf
Eugene Lanets <swarrt@yandex.ru>
2022-08-27 17:52:23 +0300

    feat: remove node-telegram-bot-api add Telegraf

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf, Markup } = require('telegraf');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const {TOKEN: token} = process.env;
-const bot = new TelegramBot(token, {polling: true});
 
-bot.on("message", (message)=> {
-  const chatId = message.chat.id;
-  bot.sendMessage(chatId,"Вижу вас как наяву! Чем займёмся?");
+const bot = new Telegraf(token);
+
+bot.start((ctx) => {
+  console.log(ctx.update.message.from);
+  ctx.reply('hi');
 })
+
+bot.launch();
